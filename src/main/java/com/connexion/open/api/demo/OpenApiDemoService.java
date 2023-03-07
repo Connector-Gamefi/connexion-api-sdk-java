5504a3a55df55d658876ae5b6a7bb9bc94b6bf7f
evil <279610272@qq.com>
2023-03-07 14:25:53 +0800

    update file
    update file

--- src/main/java/com/connexion/open/api/demo/OpenApiDemoService.java
+++ src/main/java/com/connexion/open/api/demo/OpenApiDemoService.java
@@ -10,26 +10,26 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * OpenApi 请求service示例
+ * OpenApi request service example
  * @author connexion
  */
 @Slf4j
 @Service
 public class OpenApiDemoService {
 
-    //根据不同环境选择不同domain，此处为线上domain
+    //Select different domains according to different environments, here is online domain
     private static final String OPEN_API_DOMAIN = "https://openapi.connexion.games";
 
     /**
-     * Get 方式请求
+     * Get method
      */
     public void requestGetOpenApi(){
-        //需要请求的接口
+        //Interface required
         String url = OPEN_API_DOMAIN.concat("/global/topList/roleLevel");
         Map<String, Object> req = new HashMap<>();
-        //请求参数
+        //Request parameters
         req.put("topNum", "5");
-        //时间戳必填(此处是一个示例)实际参数为unix timestamp 秒级
+        //Timestamp is required (here is an example). The actual parameter is unix timestamp in seconds
         req.put("timestamp", 1675998834);
 
         //API_KEY、SECRET
@@ -38,8 +38,8 @@ public class OpenApiDemoService {
         String signResult = SignUtils.sign((HashMap<String, Object>) req, "a05315753c2842598ee5daca4f7ef399");
         headers.put(HeaderConstants.SIGNATURE_KEY, signResult);
         log.info(signResult);
-        //输出结果  40883fa6390c7060a874f2a310144af92586e7fe60ea9ae7adea4d4908146f94
-        //请求内容
+        //Output result  40883fa6390c7060a874f2a310144af92586e7fe60ea9ae7adea4d4908146f94
+        //Request content
         String res = HttpRequest.get(url).form(req).addHeaders(headers).execute().body();
         log.info(res);
     }
